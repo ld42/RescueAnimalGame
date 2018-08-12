2e8179df3f5aaee72ea56a12bf5086e3f6ede5d0
Flaurens <lorenzonapl@hotmail.com>
2018-08-12 12:22:35 +0200

    Fix coin animation

--- GameProject/Assets/Scripts/IncreaseCoinTestScript.cs
+++ GameProject/Assets/Scripts/IncreaseCoinTestScript.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class IncreaseCoinTestScript : MonoBehaviour {
 
+    public Animator anim;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void Press()
+    {
+        anim.SetTrigger("press");
+    }
+
     public void AddCoin()
     {
         CoinQtyScript.coinQuantity += 1;
