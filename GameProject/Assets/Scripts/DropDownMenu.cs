fe21921522a3641f70d02aff3a68ae9d51f65faa
Flaurens <lorenzonapl@hotmail.com>
2018-08-12 20:29:12 +0200

    Add Activities menu

---
+++ GameProject/Assets/Scripts/DropDownMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropDownMenu : MonoBehaviour {
+
+    public Animator anim;
+    private bool showMenu;
+
+	// Use this for initialization
+	void Start () {
+        showMenu = false;
+
+	}
+	
+	public void DropDown () {
+		if(showMenu == true)
+        {
+            anim.SetBool("showMenu", false);
+            showMenu = false;
+        }
+        else
+        {
+            anim.SetBool("showMenu", true);
+            showMenu = true;
+        }
+	}
+}
