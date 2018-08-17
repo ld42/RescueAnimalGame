c3c6982fb9ca1f28d9582c7805e8aadfa812e80d
Lorenzo.navarro <lorenzo.navarro@sf.zetes.com>
2018-08-17 12:03:22 +0200

    Add disable buff option and rewards

--- GameProject/Assets/Scripts/BuffAnimationController.cs
+++ GameProject/Assets/Scripts/BuffAnimationController.cs
@@ -9,10 +9,26 @@ public class BuffAnimationController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         anim = GetComponent<Animator>();
+        //get animal count
 	}
 	
 	public void PressEventButton () {
         //anim.SetTrigger("activateBuff");
         anim.SetBool("activeBuff", true);
 	}
+
+    public void EndBuff()
+    {
+        if (anim.GetBool("activeBuff") != false)
+        {
+            anim.SetBool("activeBuff", false);
+            if (AnimalQtyScript.animalQty > 0)
+            {
+                //TODO
+                //Make animalQty and moneyToEarn a manager var in order to choose qty
+                AnimalQtyScript.animalQty -= 1;
+            }
+            CoinQtyScript.coinQuantity += 10;
+        }
+    }
 }
